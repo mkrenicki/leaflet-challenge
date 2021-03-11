b75cf4b80a1b201f3a339022b71893835921157a
mkrenicki <73031837+mkrenicki@users.noreply.github.com>
2021-03-11 13:05:45 -0500

    setting up logic.js

---
+++ static/js/logic.js
@@ -0,0 +1,23 @@
+// Level 1: Basic Visualization
+
+// Pick dataset from http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+// Chosen data set (JSON URL): https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson
+// Data set is for all earthquakes with magnitude over 4.5 in past week
+// Assign link to variable
+var equakeJSON = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
+
+// Create a map using Leaflet that plots all of the earthquakes from your data set based on their longitude and latitude.
+
+
+// Your data markers should reflect the magnitude of the earthquake by their size and and depth of the earth quake by color.
+
+
+// Earthquakes with higher magnitudes should appear larger and earthquakes with greater depth should appear darker in color.
+
+
+// Include popups that provide additional information about the earthquake when a marker is clicked.
+
+
+// Create a legend that will provide context for your map data.
+
+
